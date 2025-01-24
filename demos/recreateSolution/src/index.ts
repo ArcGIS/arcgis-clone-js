b57c575113b54768f662313043256a05596a8743
Previn Wong <PWong@esri.com>
2025-01-24 15:16:25 -0800

    create new recreate demo
    made a new demo for recreate workflow.
    
    also reverted create demo.

--- demos/recreateSolution/src/index.ts
+++ demos/recreateSolution/src/index.ts
@@ -54,21 +54,11 @@ function go () {
     portal: srcPortal
   });
 
-  // Dest credentials  (uncomment this and html if you want to target different org for test purproses)
-  const destHtmlValue = htmlUtil.getHTMLValue("destPortal");
-  const destPortalStr = destHtmlValue.endsWith('/') ? destHtmlValue.slice(0, -1) : destHtmlValue;
-  const destPortal = (destPortalStr || "https://www.arcgis.com") + "/sharing/rest";
-  const destCreds = new common.UserSession({
-    username: htmlUtil.getHTMLValue("destUsername"),
-    password: htmlUtil.getHTMLValue("destPassword"),
-    portal: destPortal
-  });
-
   // Create!
   main.createSolution(
     id,
     srcCreds,
-    destCreds,
+    srcCreds,
     percentDone => {
       document.getElementById("output").innerHTML = "Creating..." + percentDone.toFixed().toString() + "%";
     },
